97bfc3ccb6d95a7083c324cd165bcd1a84aa432b
Eshratjahan17 <eshratjahan701@gmail.com>
2022-02-13 23:01:49 +0600

    pin matched

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//random number genarate
 function pingenerate() {
     let randomGenarate = Math.round(Math.random() * 10000);
     let pinString = randomGenarate + '';
@@ -19,4 +20,46 @@ document.getElementById('pin-genarate-button').addEventListener('click', functio
     document.getElementById('pin-genarate-field').value = genetaringPin;
 
 
-})
+});
+
+//calculator handle
+document.getElementById('key-pads').addEventListener('click', function(event) {
+    const inputNumber = event.target.innerText;
+    const previousInput = document.getElementById('input-key');
+    if (isNaN(inputNumber)) {
+        if (inputNumber == 'C') {
+            previousInput.value = '';
+
+        }
+
+    } else {
+        const previousNumber = previousInput.value;
+        const newNumber = previousNumber + inputNumber;
+        previousInput.value = newNumber;
+
+
+
+    }
+    // if (document.getElementById('pin-genarate-field').value = previousInput.value) {
+    //     const notMatched = document.getElementById('did-not-matched');
+    //     notMatched.style.display = 'none';
+    // }
+
+
+
+});
+//pin matched
+function verify() {
+    const finalPin = document.getElementById('pin-genarate-field').value;
+    const typedPin = document.getElementById('input-key').value;
+    const success = document.getElementById('matched');
+    const failed = document.getElementById('failed');
+    if (finalPin == typedPin) {
+        success.style.display = 'block';
+        failed.style.display = 'none';
+    } else {
+        success.style.display = 'none';
+        failed.style.display = 'block';
+    }
+
+}
